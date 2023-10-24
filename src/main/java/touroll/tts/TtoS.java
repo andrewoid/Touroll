8330f28243963cd4ec3c2283d9a54229ce32f65a
emmyrunge <erunge@student.touro.edu>
2023-10-23 21:42:16 -0400

    added ability to put text to an audio file and test for it

--- src/main/java/touroll/tts/TtoS.java
+++ src/main/java/touroll/tts/TtoS.java
@@ -1,10 +1,16 @@
 package touroll.tts;
 
+import com.sun.speech.freetts.audio.SingleFileAudioPlayer;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.LineUnavailableException;
 import javax.speech.AudioException;
 import javax.speech.Central;
 import javax.speech.EngineException;
 import javax.speech.synthesis.Synthesizer;
 import javax.speech.synthesis.SynthesizerModeDesc;
+import java.io.IOException;
 import java.util.Locale;
 
 public class TtoS
@@ -21,7 +27,7 @@ public class TtoS
                         + ".jsapi.FreeTTSEngineCentral");
     }
 
-    public void speakAudioOfText(String textForAudio) throws EngineException, AudioException, InterruptedException {
+    public void speakAudioOfText(String textForAudio) throws EngineException, AudioException, InterruptedException, LineUnavailableException, IOException {
         // Create a Synthesizer
         Synthesizer synthesizer
                 = Central.createSynthesizer(
@@ -33,9 +39,16 @@ public class TtoS
         // Resume Synthesizer
         synthesizer.resume();
 
+        SingleFileAudioPlayer singleFileAudioPlayer = new SingleFileAudioPlayer("textToFile", AudioFileFormat.Type.WAVE);
+        singleFileAudioPlayer.setAudioFormat(new AudioFormat(44100, 16, 2, true, true));
+
+        singleFileAudioPlayer.begin(1000);
+
         // Speaks the given text until the queue is empty.
         synthesizer.speakPlainText(textForAudio, null);
         synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
+
+        singleFileAudioPlayer.close();
 
         // Deallocate the Synthesizer.
         synthesizer.deallocate();
--- src/test/java/touroll/tts/TtoSTest.java
+++ src/test/java/touroll/tts/TtoSTest.java
@@ -2,21 +2,41 @@ package touroll.tts;
 
 import org.junit.jupiter.api.Test;
 
+import javax.sound.sampled.LineUnavailableException;
 import javax.speech.AudioException;
 import javax.speech.EngineException;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TtoSTest
 {
 
     @Test
-    public void speakAudioOfText() throws EngineException, AudioException, InterruptedException {
+    public void speakAudioOfText() throws EngineException, AudioException, InterruptedException, LineUnavailableException, IOException {
         //given
         TtoS tts = new TtoS();
 
         //when
-        tts.speakAudioOfText("Hey michal how are you doing today");
+        tts.speakAudioOfText("Scooby Dooby Doo, where are you?\n" +
+                "We got some work to do now\n" +
+                "Scooby Dooby Doo, where are you?");
+        //then
+
+    }
+
+    @Test
+    public void checkForFile() {
+        //given
+        String wavFileName = "textToFile.wav";
+
+        //when
+        File wavFile = new File("textToFile.wav", wavFileName);
 
         //then
+        //check for existing file
+        assertTrue(wavFile.exists(), "Hey down here: the .wav file was not created");
 
     }
 }
