676f7d78f225c92db7e150c0d12bd5091271da7d
ElkySchles <elbrsc523@gmail.com>
2023-10-31 19:42:16 -0400

    Added newly modified UI to resources

--- src/test/java/touroll/markdown/MarkdownRendererTest.java
+++ src/test/java/touroll/markdown/MarkdownRendererTest.java
@@ -1,16 +1,13 @@
 package touroll.markdown;
 
 import org.junit.jupiter.api.Test;
-import touroll.markdown.MarkdownRenderer;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MarkdownRendererTest
-{
+class gitMarkdownRendererTest {
 
     @Test
-    public void getHtmlRendering()
-    {
+    public void getHtmlRendering() {
         MarkdownRenderer markdownRenderer = new MarkdownRenderer();
         String actual = markdownRenderer.getHtmlRendering("What is *this*?");
         String expected = "<p>What is <em>this</em>?</p>\n";
