8a3dea8dd9668b645d356605cf193788ae16035a
MC-Student <meiratova123@gmail.com>
2023-11-01 00:42:46 -0400

    createImageFileFromHtmlString should return an image with transparent background. Current test still passes. Note that the image transparency also depends on the way it is written through ImageIO; see comment preceding method

--- src/main/java/touroll/markdown/HtmlToImageRenderer.java
+++ src/main/java/touroll/markdown/HtmlToImageRenderer.java
@@ -10,7 +10,8 @@ public class HtmlToImageRenderer
     private final int width;
     private final int height;
     private final BufferedImage image;
-    private final Graphics graphics;
+    //private final Graphics graphics;
+    private final Graphics2D graphics2D;
     private final JEditorPane jep;
 
     public HtmlToImageRenderer() throws IOException
@@ -19,16 +20,25 @@ public class HtmlToImageRenderer
         height = 600;
         image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         //https://stackoverflow.com/questions/10391778/create-a-bufferedimage-from-file-and-make-it-type-int-argb
-        graphics = image.createGraphics();
+        //graphics = image.createGraphics();
+        graphics2D = image.createGraphics();
+        graphics2D.setComposite(AlphaComposite.Clear);
+        graphics2D.fillRect(0, 0, width, height);
+        //For above 3 lines used:
+        // https://stackoverflow.com/questions/17271812/save-buffered-image-with-transparent-background
         jep = new JEditorPane("text/html", "");
         //Used https://stackoverflow.com/questions/59160419/java-convert-html-to-image
     }
+
+    //IMPORTANT: When this method is used, the Image should be output in a png format
+    //Why? To maintain transparent background
+    //For example: ImageIO.write(bufferedImage, "png", file);
 
     public Image createImageFileFromHtmlString(String html)
     {
         jep.setText(html);
         jep.setSize(width, height);
-        jep.print(graphics);
+        jep.print(graphics2D);
         return image;
     }
 }
