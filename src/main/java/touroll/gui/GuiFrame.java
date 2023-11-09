eaff7a19d8ba5cd87c4dc7053ada84ad3da3acb6
ElkySchles <elbrsc523@gmail.com>
2023-11-09 14:15:08 -0500

    Updated main page of Gui

--- src/main/java/touroll/gui/GuiFrame.java
+++ src/main/java/touroll/gui/GuiFrame.java
@@ -4,13 +4,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GuiFrame extends JFrame {
+    private JButton element = new JButton();
 
-    private final JScrollPane scrollPane;
-    private final JList<String> list;
+
     final ListModel<String> listModel = new DefaultListModel<>();
 
 
     public GuiFrame() {
+        final JScrollPane scrollPane;
+        final JList<String> list;
+
         //main GUI frame
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
@@ -21,40 +24,45 @@ public class GuiFrame extends JFrame {
 
         //add the list to the main panel
         list = new JList<>(listModel);
-        mainPanel.add(list);
         scrollPane = new JScrollPane(list);
-        mainPanel.add(scrollPane, BorderLayout.WEST);
 
-        //panel to add Frame length and width labels and buttons
+
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+
+        //panel to add length and width labels and buttons
         JPanel buttonPanel = new JPanel(new BorderLayout());
 
-        //top button to add element
-        JButton element = new JButton("Add element");
+        //north button to add element
+        element = new JButton("Add Element");
+        element.setPreferredSize(new Dimension(200, 65));
         buttonPanel.add(element, BorderLayout.NORTH);
 
-        //panel to set the width and height
-        JPanel dimensionPanel = new JPanel(new GridLayout(2, 3));
-        JLabel width = new JLabel("width");
-        JTextField widthText = new JTextField();
-        JButton submitWidth = new JButton("submit");
-        JLabel height = new JLabel("height");
-        JTextField heightText = new JTextField();
-        JButton submitHeight = new JButton("submit");
-
-        dimensionPanel.add(width);
-        dimensionPanel.add(widthText);
-        dimensionPanel.add(submitWidth);
-        dimensionPanel.add(height);
-        dimensionPanel.add(heightText);
-        dimensionPanel.add(submitHeight);
-        buttonPanel.add(dimensionPanel, BorderLayout.CENTER);
-
-        //button to generate video
+        //south button to generate video
         JButton generate = new JButton("Generate video");
+        generate.setPreferredSize(new Dimension(200, 65));
         buttonPanel.add(generate, BorderLayout.SOUTH);
 
+        //panel to set the width and height
+        JPanel sizingPanel = new JPanel();
+        sizingPanel.setLayout(new BoxLayout(sizingPanel, BoxLayout.Y_AXIS));
+
+        JLabel width = new JLabel("Width");
+        JTextField widthText = new JTextField();
+
+
+        JLabel height = new JLabel("Height");
+        JTextField heightText = new JTextField();
+
+        sizingPanel.add(width);
+        sizingPanel.add(widthText);
+        sizingPanel.add(height);
+        sizingPanel.add(heightText);
+        buttonPanel.add(sizingPanel, BorderLayout.CENTER);
+
+
         mainPanel.add(buttonPanel, BorderLayout.EAST);
+
+
     }
 
-    //add dialouge box for adding a video element
 }
