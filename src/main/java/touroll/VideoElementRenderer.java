f7adb3630fbb53faa898afc59778dfab8b0331e7
unknown <elkyhalberstam@gmail.com>
2023-11-15 16:04:52 -0500

    adding videRenderere and VideoRenderer test (also audio files 1 & 2)

--- src/main/java/touroll/VideoElementRenderer.java
+++ src/main/java/touroll/VideoElementRenderer.java
@@ -1,25 +1,65 @@
 package touroll;
 
+import touroll.image.ImageElement;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VideoElementRenderer {
 
-    public void render(List<VideoElement> list) throws IOException {
-        // > ffmpeg -r FPS -i input.txt -pix_fmt yuv420p output.mp4
+    public List<String> render(List<VideoElement> list) throws IOException {
 
-        PrintWriter writer = new PrintWriter(new File("input.txt"));
+        List<String> retval =  new ArrayList<>();
+
+        File imageFile = new File("src/test/resources/IHaveADream/imageInput.txt");
+        File audioFile =new File("src/test/resources/IHaveADream/audioInput.txt");
+        File outputPart1 = new File("src/test/resources/IHaveADream/outputPart1.mv4");
+        File output = new File("src/test/resources/IHaveADream/output.mv4");
+
+        PrintWriter writer1 = new PrintWriter(imageFile);
+        PrintWriter writer2 = new PrintWriter(audioFile);
 
         list.forEach(element -> {
-            writer.print("file '");
-            writer.print(element.getOutputFile());
-            writer.println("'");
+            if(element instanceof ImageElement) {
+                writer1.print("file '");
+                writer1.print(element.getOutputFile());
+                writer1.println("'");
+            }
+            //dont have a speach element yet
+            /*else if (element instanceof SpeechElement){
+                writer2.print("file '");
+                writer2.print(element.getOutputFile());
+                writer2.println("'");
+            }*/
         });
+        writer1.close();
+        writer2.close();
 
-        writer.close();
+        String imageTOvideo = "ffmpeg -f concat -i " +imageFile
+                +" -c:v libx264 -r 1/5 -pix_fmt yuv420p " +outputPart1;
+        //dont have a speach element yet
+        /* String outputWithAudio = "ffmpeg  -i " +outputPart1 +"-f concat -i " +audioFile
+                +" -c:v libx264 -pix_fmt yuv420p " +output; */
+
+        retval.add(imageTOvideo);
+        //dont have a speach element yet
+        //retval.add(outputWithAudio);
+
+        return retval;
     }
-
-
 }
+
+// 2 commands....
+//ffmpeg -f concat -i input2.txt -c:v libx264 -r 1/5 -pix_fmt yuv420p output1.mp4
+//ffmpeg  -i output1.mp4 -f concat -i input3.txt -c:v libx264 -pix_fmt yuv420p output2.mp4
+// out puts a file with ffmpeg inputs
+// > ffmpeg -r FPS -i input.txt -pix_fmt yuv420p output.mp4
+// make sure it output a proper working ffmpeg command for CP
+//      --new method or class? zFFMpeg render
+// given a frames per second
+// given input file
+// given an audio file
+// take in name of output
---
+++ src/test/java/touroll/VideoElementRendererTest.java
@@ -0,0 +1,40 @@
+package touroll;
+
+import org.junit.jupiter.api.Test;
+import touroll.image.ImageElement;
+import touroll.image.ImageRenderer;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.function.BooleanSupplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class VideoElementRendererTest {
+
+    @Test
+    void render() throws IOException {
+        //given
+        File source = new File("src/test/resources/IHaveADream/img1.jpg");
+        ImageElement element = mock();
+        doReturn(source).when(element).getOutputFile();
+        ArrayList<VideoElement> videoElements = new ArrayList<>();
+        videoElements.add(element);
+
+        //when
+        VideoElementRenderer videoElementRenderer = new VideoElementRenderer();
+        ArrayList<String> ffmpegCommands = (ArrayList<String>) videoElementRenderer.render(videoElements);
+
+        //then
+
+        assertTrue(Files.isRegularFile(Path.of("src/test/resources/IHaveADream/imageInput.txt")));
+        assertTrue(Files.isRegularFile(Path.of("src/test/resources/IHaveADream/audioInput.txt")));
+
+        assertEquals(ffmpegCommands.get(0), "ffmpeg -f concat -i src\\test\\resources\\IHaveADream\\imageInput.txt " +
+                "-c:v libx264 -r 1/5 -pix_fmt yuv420p src\\test\\resources\\IHaveADream\\outputPart1.mv4");
+    }
+}
